7fd5c758f0da8efd659728fae590f3a56a55c903
Caio Peternela <caiopeternela.dev@gmail.com>
2022-08-30 15:17:58 -0300

    adding check task feature

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,14 +2,20 @@
     <div class="tasks">
         <h1>Vuelist</h1>
         <div class="new">
-            <input v-if="!editableMode" v-model="task" type="text" name="new" id="new" placeholder="Add a new task">
-            <input v-else v-model="task" type="text" name="new" id="new" placeholder="Edit the task">
-            <button v-if="!editableMode" @click="addTask()" class="new-button">Add</button>
-            <button v-else @click="editTask(), editStatus()" class="new-button">Edit</button>
+            <input v-if="!editableMode" @keyup.enter="addTask()" v-model="task" type="text" name="new" class="input" :placeholder="placeholderMsg">
+            <input v-else v-model="task" @keyup.enter="editTask()" type="text" name="new" class="input edit-input" :placeholder="placeholderMsg">
+            <button v-if="!editableMode" @click="addTask()" id="add-button" class="new-button">Add</button>
+            <button v-else @click="editTask(), editStatus()" class="new-button edit-button">Edit</button>
         </div>
         <ul class="tasks-list">
-            <li v-for="task in tasks" :key="task.id">{{ task.task }}<span @click="editStatus(task.id)" class="edit"><font-awesome-icon icon="fas fa-edit" /></span>
-            <span @click="deleteTask(task.id)" class="delete"><font-awesome-icon icon="fa-trash-can" /></span></li>
+            <li v-for="task in tasks" :key="task.id">
+                <span v-if="task.completed == false" @click="editCompleted(task.id)" class="status"><font-awesome-icon icon="fa-solid fa-check"/></span>
+                <span v-else @click="editCompleted(task.id)" class="status"><font-awesome-icon icon="fas fa-undo"/></span>
+                <span v-if="task.completed == false">{{ task.task }}</span>
+                <span v-else class="task-tag">{{ task.task }}</span>
+                <span @click="editStatus(task.id)" class="edit"><font-awesome-icon icon="fas fa-edit"/></span>
+                <span @click="deleteTask(task.id)" class="delete"><font-awesome-icon icon="fa-trash-can"/></span>
+            </li>
         </ul>
     </div>
 </template>
@@ -22,20 +28,26 @@
                 tasks: null,
                 task: null,
                 id: null,
-                editableMode: false
+                editableMode: false,
+                completed: false,
+                placeholderMsg: "Add a new task"
             }
         },
         methods: {
             async addTask() {
-                const dataJson = JSON.stringify({task: this.task})
-                const req = await fetch("http://localhost:3000/tasks", {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body: dataJson
-                })
-                const res = await req.json()
-                this.getTasks()
-                this.task = null
+                if (this.task != null) {
+                    const dataJson = JSON.stringify({task: this.task, completed: false})
+                    const req = await fetch("http://localhost:3000/tasks", {
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        body: dataJson
+                    })
+                    const res = await req.json()
+                    this.getTasks()
+                    this.task = null
+                } else {
+                    this.placeholderMsg = "Invalid task"
+                }
             },
             async getTasks() {
                 const req = await fetch("http://localhost:3000/tasks")
@@ -56,17 +68,42 @@
             editStatus(id) {
                 this.editableMode = !this.editableMode
                 this.id = id
+                if (this.editableMode) {
+                    this.placeholderMsg = "Edit task"
+                }
             },
             async editTask() {
-                const newTask = this.task
-                const dataJson = JSON.stringify({task: newTask})
-                const req = await fetch(`http://localhost:3000/tasks/${this.id}`, {
+                if (this.task != null) {
+                    const newTask = this.task
+                    const dataJson = JSON.stringify({task: newTask})
+                    const req = await fetch(`http://localhost:3000/tasks/${this.id}`, {
+                        method: "PATCH",
+                        headers: {"Content-Type": "application/json"},
+                        body: dataJson
+                    })
+                    const res = await req.json()
+                    this.task = null
+                    this.getTasks()
+                    this.id = null
+                } else {
+                    this.placeholderMsg = "Invalid task"
+                }
+            },
+            async editCompleted(id) {
+                const get = await fetch(`http://localhost:3000/tasks/${id}`)
+                const data = await get.json()
+                let dataJson
+                if (data.completed == true) {
+                    dataJson = JSON.stringify({completed: false})
+                } else {
+                    dataJson = JSON.stringify({completed: true})
+                }
+                const req = await fetch(`http://localhost:3000/tasks/${id}`, {
                     method: "PATCH",
                     headers: {"Content-Type": "application/json"},
                     body: dataJson
                 })
                 const res = await req.json()
-                this.task = null
                 this.getTasks()
             }
         },
@@ -78,9 +115,10 @@
 
 <style scoped>
     .tasks {
-        margin: 120px auto;
+        margin: 50px auto;
         max-width: 450px;
         width: 100%;
+        height: fit-content;
         background: #41B883;
         color: white;
         border-radius: 5px;
@@ -99,17 +137,22 @@
         margin: 20px 0;
     }
 
-    .new input {
+    .input {
         outline: none;
         width: 85%;
         height: 100%;
         border: 3px solid #35495E;
         font-size: 16px;
         border-radius: 3px;
-        padding-left: 15px;
+        padding-left: 15px; 
     }
 
-    .new button {
+    .edit-input {
+        border: 3px solid #278cf7;
+        color: black;
+    }
+
+    .new-button {
         width: 60px;
         height: 100%;
         border: none;
@@ -122,6 +165,15 @@
         margin-left: 5px;
     }
 
+    .edit-button {
+        background: #278cf7;
+    }
+
+    .tasks-list {
+        max-height: 580px;
+        overflow: auto;
+    }
+
 
     .tasks-list li {
         list-style: none;
@@ -129,9 +181,14 @@
         line-height: 45px;
         position: relative;
         background: #35495E;
+        margin-right: 15px;
         margin-bottom: 8px;
-        padding: 0 15px;
+        padding: 0 55px;
+        font-size: 20px;
         border-radius: 3px;
+        -webkit-user-select: none; 
+        -ms-user-select: none; 
+        user-select: none;
     }
 
     .edit {
@@ -143,6 +200,21 @@
         cursor: pointer;
     }
 
+    .task-tag {
+        text-decoration: line-through
+    }
+
+    .status {
+        position: absolute;
+        left: 0px;
+        background: white;
+        text-align: center;
+        border-radius: 3px 0 0 3px;
+        width: 45px;
+        cursor: pointer;
+        color: black;
+    }
+
     .delete {
         position: absolute;
         right: 0px;
@@ -151,6 +223,24 @@
         border-radius: 0 3px 3px 0;
         width: 45px;
         cursor: pointer;
+    }
+
+
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: transparent; 
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: #35495E;
+        border-radius: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: #35495E; 
     }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEdit, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTrashCan, faCheck, faUndo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faEdit, faTrashCan)
+library.add(faEdit, faTrashCan, faCheck, faUndo)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
